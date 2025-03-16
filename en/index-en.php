22c33b9e4dfdb13ef59ba05e838c22f6c3942b87
sompong59 <102735484+sompong59@users.noreply.github.com>
2025-03-16 23:57:37 +0700

    Add files via upload

---
+++ en/index-en.php
@@ -0,0 +1,312 @@
+<?php $pageTitle = "TECHART Foreign Language Translation Services Company Limited"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $pageTitle; ?></title>
+    <link rel="shortcut icon" href="../img/logo.jpeg" type="logo">
+    <link rel="stylesheet" href="../css/process.css">
+    <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="../css/service.css">
+    <link rel="stylesheet" href="../css/hero.css">
+    <link rel="stylesheet" href="../css/customer.css">
+    <link rel="stylesheet" href="../css/social-buttons.css"> 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+</head>
+
+<body>
+<?php include 'header-en.html';?>
+
+<section class="herologo">
+    <div class="hero-image">
+        <img src="../img/logo.jpeg" alt="TECHART Logo">
+    </div>
+    <div class="hero-content">
+        <div class="company-logo">
+            <img src="../img/trans.png" alt="Logo">
+        </div>
+        <h1>"Accuracy, Trust, and Speed: Let us translate for you."</h1>
+        <button class="consultation-btn">Initial Consultation</button>
+        <div class="hero-social-icons">
+            <a href="#"><i class="hero-icon-instagram"></i></a>
+            <a href="#"><i class="hero-icon-facebook"></i></a>
+            <a href="#"><i class="hero-icon-twitter"></i></a>
+        </div>
+    </div>
+</section>
+
+<section class="about-section">
+    <div class="container">
+        <div class="heading-container text-center">
+            <h2>About <span>TECHART Company</span></h2>
+            <p>
+                TECHART Foreign Language Translation Services Sole Company Limited was established legally under the Enterprise Law No. 46/NA, dated 26/12/2013 and the Publishing Law No. 05/NA, dated 09/12/2008. Operating under the supervision of the Ministry of Information, Culture and Tourism and the regulations of the Lao PDR issued periodically. Our company is a leading provider of document translation, review, and certification services for foreign languages, supported by a team with over 5 years of experience and advanced degrees (Master's and above).
+            </p>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="image-box">
+                    <img src="../img/about.png" alt="About TECHART Company">
+                </div>
+            </div>
+            <div class="col-md-6">
+                <h2>Vision and Mission</h2>
+                <div class="details-box">
+                    <p>
+                        <strong>Vision:</strong> To be a trusted company providing satisfying document and foreign language translation services to our clients.
+                    </p>
+                    <p>
+                        <strong>Mission:</strong> To emphasize and adhere to professional translation services, create a lasting impression for all customers, and strive to develop our personnel and language service tools to be modern and responsive to the evolving landscape.
+                    </p>
+                    <a href="maps.html" class="btn btn-primary">Contact Us</a>
+                    <div class="social-icons">
+                        <div class="social-icons">
+                            <a href="#" class="social-icon" title="WhatsApp"><i class="fab fa-whatsapp"></i></a>
+                            <a href="#" class="social-icon" title="Facebook"><i class="fab fa-facebook-f"></i></a>
+                            <a href="mailto:your@email.com" class="social-icon" title="Email"><i class="fas fa-envelope"></i></a>
+                            <a href="#" class="social-icon" title="TikTok"><i class="fab fa-tiktok"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<div class="service-container">
+    <div class="translate-top-section">
+        <div class="translate-top-section-content">
+            <h2>What is Certified Document Translation?</h2>
+            <p>Certified document translation is for documents used in official settings where the recipient requires assurance of accuracy and completeness. These documents are often used in various locations such as immigration offices, government institutions, and other local and international venues.</p>
+        </div>
+    </div>
+    <div class="containercard">
+        <div class="service-card">
+            <h2>Common Document Types</h2>
+            <ul>
+                <li><i class="fas fa-check-circle"></i> ID Card</li>
+                <li><i class="fas fa-check-circle"></i> Family Book</li>
+                <li><i class="fas fa-check-circle"></i> Business Registration Certificate</li>
+                <li><i class="fas fa-check-circle"></i> Investment License</li>
+                <li><i class="fas fa-check-circle"></i> Address Certificate</li>
+                <li><i class="fas fa-check-circle"></i> Marriage Certificate</li>
+                <li><i class="fas fa-check-circle"></i> Application Form</li>
+                <li><i class="fas fa-check-circle"></i> Various Licenses</li>
+                <li><i class="fas fa-check-circle"></i> Tax Invoice</li>
+                <li><i class="fas fa-check-circle"></i> Employee Certificate</li>
+            </ul>
+            <button class="quote-button">Get a Free Quote</button>
+        </div>
+        <div class="service-card">
+            <h2>Various Form Types</h2>
+            <ul>
+                <li><i class="fas fa-check-circle"></i> Criminal Record Check</li>
+                <li><i class="fas fa-check-circle"></i> Student CV</li>
+                <li><i class="fas fa-check-circle"></i> Employee CV</li>
+                <li><i class="fas fa-check-circle"></i> Birth Certificate</li>
+                <li><i class="fas fa-check-circle"></i> Criminal Record Check</li>
+                <li><i class="fas fa-check-circle"></i> Guardianship Certificate</li>
+                <li><i class="fas fa-check-circle"></i> Guarantee Letter</li>
+                <li><i class="fas fa-check-circle"></i> Government Document Forms</li>
+                <li><i class="fas fa-check-circle"></i> Company Document Forms</li>
+                <li><i class="fas fa-check-circle"></i> Domestic and International Visa Application Forms</li>
+            </ul>
+            <button class="quote-button">Get a Free Quote</button>
+        </div>
+        <div class="service-card">
+            <h2>Contract Document Types</h2>
+            <ul>
+                <li><i class="fas fa-check-circle"></i> Project Concession Agreement</li>
+                <li><i class="fas fa-check-circle"></i> Power Purchase Agreement</li>
+                <li><i class="fas fa-check-circle"></i> Shareholder Agreement</li>
+                <li><i class="fas fa-check-circle"></i> Share Purchase Agreement</li>
+                <li><i class="fas fa-check-circle"></i> Company Bylaws</li>
+                <li><i class="fas fa-check-circle"></i> Service Agreement</li>
+                <li><i class="fas fa-check-circle"></i> Consultant Agreement</li>
+                <li><i class="fas fa-check-circle"></i> Construction Agreement</li>
+                <li><i class="fas fa-check-circle"></i> Project Development Agreement</li>
+                <li><i class="fas fa-check-circle"></i> Memorandum of Understanding</li>
+            </ul>
+            <button class="quote-button">Get a Free Quote</button>
+        </div>
+        <div class="service-card">
+            <h2>Other Document Types</h2>
+            <ul>
+                <li><i class="fas fa-check-circle"></i> Preface</li>
+                <li><i class="fas fa-check-circle"></i> Abstract</li>
+                <li><i class="fas fa-check-circle"></i> Menu</li>
+                <li><i class="fas fa-check-circle"></i> Product/Service Manual</li>
+                <li><i class="fas fa-check-circle"></i> Advertising Media</li>
+                <li><i class="fas fa-check-circle"></i> Meeting Minutes</li>
+                <li><i class="fas fa-check-circle"></i> Passport</li>
+                <li><i class="fas fa-check-circle"></i> Invoice</li>
+                <li><i class="fas fa-check-circle"></i> Books</li>
+                <li><i class="fas fa-check-circle"></i> Documents as per customer’s request</li>
+            </ul>
+            <button class="quote-button">Get a Free Quote</button>
+        </div>
+    </div>
+</div>
+
+<h2>Supports Multiple Languages</h2>
+<nav class="language-selector">
+    <div class="language-container">
+        <a href="lao.html" class="language-item">
+            <img src="../img/Lao.png" alt="Lao Flag">
+            <span>Lao</span>
+        </a>
+        <a href="thai.html" class="language-item">
+            <img src="../img/Thai.png" alt="Thai Flag">
+            <span>Thai</span>
+        </a>
+        <a href="english.html" class="language-item">
+            <img src="../img/english.png" alt="English Flag">
+            <span>English</span>
+        </a>
+        <a href="russian.html" class="language-item">
+            <img src="../img/russia.png" alt="Russian Flag">
+            <span>Russian</span>
+        </a>
+        <a href="chinese.html" class="language-item">
+            <img src="../img/chaina.png" alt="Chinese Flag">
+            <span>Chinese</span>
+        </a>
+        <a href="vietnamese.html" class="language-item">
+            <img src="../img/vietnam.png" alt="Vietnamese Flag">
+            <span>Vietnamese</span>
+        </a>
+        <a href="french.html" class="language-item">
+            <img src="../img/franc.png" alt="French Flag">
+            <span>French</span>
+        </a>
+    </div>
+</nav>
+
+<div class="div">
+    <h2>Document Translation Process</h2>
+    <p class="pralagrap3">The document translation process consists of 4 steps, detailed as follows:</p>
+</div>
+
+<div class="card-container">
+    <div class="card">
+        <span class="card-number">1</span>
+        <img src="../img/111.png" alt="Image 1" class="card-img-top">
+        <div class="card-body">
+            <p class="card-text"><strong>Initial Inquiry:</strong><br> General information inquiry between the client and the company.</p>
+        </div>
+    </div>
+    <div class="card">
+        <span class="card-number">2</span>
+        <img src="../img/22.png" alt="Image 2" class="card-img-top">
+        <div class="card-body">
+            <p class="card-text"><strong>Price Estimation:</strong><br> The client sends documents for the company to review, estimate the translation price, determine the exact completion time, and make a 30% deposit before agreeing to translate.</p>
+        </div>
+    </div>
+    <div class="card">
+        <span class="card-number">3</span>
+        <img src="../img/33.png" alt="Image 3" class="card-img-top">
+        <div class="card-body">
+            <p class="card-text"><strong>Accuracy Verification:</strong><br> After the company completes the document translation, it is sent to the client for review to verify any unclear points for complete accuracy and 100% usability.</p>
+        </div>
+    </div>
+    <div class="card">
+        <span class="card-number">4</span>
+        <img src="../img/44.png" alt="Image 4" class="card-img-top">
+        <div class="card-body">
+            <p class="card-text"><strong>Document Handover:</strong><br> Before handing over or sending the documents to the client, the client must pay the remaining 70% before receiving the accurate translation from the company.</p>
+        </div>
+    </div>
+</div>
+
+<h2>Examples of Completed Translated Documents</h2>
+<div class="carousel-container">
+    <div class="carousel-track">
+        <div class="carousel-slide">
+            <div class="image-container">
+                <img src="../img/xp2.png" alt="Image 1">
+            </div>
+            <div class="image-container">
+                <img src="../img/xp6.png" alt="Image 2">
+            </div>
+        </div>
+        <div class="carousel-slide">
+            <div class="image-container">
+                <img src="../img/xp5.png" alt="Image 3">
+            </div>
+            <div class="image-container">
+                <img src="../img/xp6.png" alt="Image 4">
+            </div>
+        </div>
+        <div class="carousel-slide">
+            <div class="image-container">
+                <img src="../img/xp2.png" alt="Image 5">
+            </div>
+            <div class="image-container">
+                <img src="../img/xp3.png" alt="Image 6">
+            </div>
+        </div>
+    </div>
+    <div class="navigation">
+        <button class="carousel-prev">&#10094;</button>
+        <button class="carousel-next">&#10095;</button>
+    </div>
+</div>
+<div class="my-container">
+    <div class="my-header">
+        <h1>Trusted Customers</h1>
+        <div class="my-orange-bar"></div>
+    </div>
+    <div class="my-customer-grid">
+        <div class="my-customer-item">
+            <img src="../img/1.png" alt="Charoen Sekong Carbon">
+        </div>
+        <div class="my-customer-item">
+            <img src="../img/2.png" alt="Thanok Wangkao Tha Tok Company">
+        </div>
+        <div class="my-customer-item">
+            <img src="../img/3.png" alt="Houng Aloun HAL EXPRESS">
+        </div>
+        <div class="my-customer-item">
+            <img src="../img/4.png" alt="Thanok Wangkao Tha Tok LPY Company">
+        </div>
+        <div class="my-customer-item">
+            <img src="../img/5.png" alt="CS ENERGY">
+        </div>
+        <div class="my-customer-item">
+            <img src="../img/6.png" alt="Luay Huay Galaxy 18">
+        </div>
+        <div class="my-customer-item">
+            <img src="../img/7.png" alt="TMN THONGMANI TRADING">
+        </div>
+        <div class="my-customer-item">
+            <img src="../img/8.png" alt="GLC">
+        </div>
+        <div class="my-customer-item">
+            <img src="../img/9.png" alt="ANOUSITH EXPRESS">
+        </div>
+        <div class="my-customer-item">
+            <img src="../img/10.png" alt="ANOUSITH EXPRESS">
+        </div>
+        <div class="my-customer-item">
+            <img src="img/11.png" alt="Thanaleng Dry Port">
+        </div>
+        <div class="my-customer-item">
+            <img src="../img/12.png" alt="BURAPHA AGRO-FORESTRY">
+        </div>
+        <div class="my-customer-item">
+            <img src="../img/13.png" alt="MaxHemp">
+        </div>
+        <div class="my-customer-item">
+            <img src="../img/14.png" alt="CR">
+        </div>
+        <div class="my-customer-item">
+            <img src="../img/15.png" alt="The World Bank">
+        </div>
+    </div>
+</div>
+
+<?php include 'footer-en.html';?>
+<script src="../js/why.js"></script>
+<script src="../js/from2.js"></script>
+</body>
+</html>
