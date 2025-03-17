aa33aa10cad7db26f827c454aa936c0abb820481
sompong59 <102735484+sompong59@users.noreply.github.com>
2025-03-17 11:15:30 +0700

    add team-en.php

---
+++ en/team-en.php
@@ -0,0 +1,130 @@
+<?php $pageTitle = "Tech Art Company Team"; ?>
+<!DOCTYPE html>
+<html lang="la">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $pageTitle; ?></title>
+    <link rel="shortcut icon" href="css/logo.jpeg" type="logo">
+    <link rel="stylesheet" href="css/team.css">
+    <link rel="stylesheet" href="css/social-buttons.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+</head>
+<body>
+    <?php include 'header-en.html';?>
+   
+    <div class="team-body">
+    <div class="team-header">
+        <h1>Our Company <span>Team</span></h1>
+    </div>
+    <div class="team-container">
+        <div class="team-member">
+            <img src="img/t1.png" alt="Mr. Sonphet Chitthavong">
+            <h3>Mr. Sonphet Chitthavong</h3>
+            <p>CEO Director</p>
+            <p>Master's Degree in Business Administration</p>
+            <div class="team-member-social-icons">
+                <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-whatsapp"></a>
+                <a href="#" class="far fa-envelope"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-youtube"></a>
+            </div>
+        </div>
+        <div class="team-member">
+            <img src="img/t2.png" alt="Ms. Pinthong Soukphaly">
+            <h3>Ms. Pinthong Soukphaly</h3>
+            <p>Manager</p>
+            <p>Bachelor's Degree in ICT</p>
+            <div class="team-member-social-icons">
+                <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-whatsapp"></a>
+                <a href="#" class="far fa-envelope"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-youtube"></a>
+            </div>
+        </div>
+        <div class="team-member">
+            <img src="img/t3.png" alt="Mr. Aloun Nakhamhoung">
+            <h3>Mr. Aloun Nakhamhoung</h3>
+            <p>Expert in Legal and Contractual Matters (French)</p>
+            <p>Master's Degree in Law and International Relations</p>
+            <div class="team-member-social-icons">
+                <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-whatsapp"></a>
+                <a href="#" class="far fa-envelope"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-youtube"></a>
+            </div>
+        </div>
+        <div class="team-member">
+            <img src="img/t4.png" alt="Mr. Chittava Madmanisone">
+            <h3>Mr. Chittava Madmanisone</h3>
+            <p>Expert in Legal and Contractual Matters (English)</p>
+            <p>Master's Degree in Business Administration (English)</p>
+            <div class="team-member-social-icons">
+                <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-whatsapp"></a>
+                <a href="#" class="far fa-envelope"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-youtube"></a>
+            </div>
+        </div>
+        <div class="team-member">
+            <img src="img/t5.png" alt="Mr. Boukham">
+            <h3>Mr. Boukham</h3>
+            <p>Vietnamese Language Expert</p>
+            <p>Master's Degree in Vietnamese Literature</p>
+            <div class="team-member-social-icons">
+                <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-whatsapp"></a>
+                <a href="#" class="far fa-envelope"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-youtube"></a>
+            </div>
+        </div>
+        <div class="team-member">
+            <img src="img/t6.png" alt="Mr. Bouaphan Yajongtor">
+            <h3>Mr. Bouaphan Yajongtor</h3>
+            <p>Chinese Language Expert</p>
+            <p>Master's Degree in Chinese Literature</p>
+            <div class="team-member-social-icons">
+                <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-whatsapp"></a>
+                <a href="#" class="far fa-envelope"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-youtube"></a>
+            </div>
+        </div>
+        <div class="team-member">
+            <img src="img/t7.png" alt="Mr. Thitsanou Xailengsavath">
+            <h3>Mr. Thitsanou Xailengsavath</h3>
+            <p>Expert in Legal and Contractual Matters (Chinese)</p>
+            <p>Master's Degree in Political Science and International Relations (China)</p>
+            <div class="team-member-social-icons">
+                <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-whatsapp"></a>
+                <a href="#" class="far fa-envelope"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-youtube"></a>
+            </div>
+        </div>
+        <div class="team-member">
+            <img src="img/t8.png" alt="Ms. Lamthian Khutphilavanh">
+            <h3>Ms. Lamthian Khutphilavanh</h3>
+            <p>Chinese Language Expert</p>
+            <p>Master's Degree in Economics and Finance</p>
+            <div class="team-member-social-icons">
+                <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-whatsapp"></a>
+                <a href="#" class="far fa-envelope"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-youtube"></a>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include 'footer-en.html';?>
+</body>
+</html>
