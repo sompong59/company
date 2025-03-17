d4f7988680dc347353118097aac60d469e2c1677
sompong59 <102735484+sompong59@users.noreply.github.com>
2025-03-17 09:22:20 +0700

    ad en about

---
+++ en/about-en.php
@@ -0,0 +1,61 @@
+<?php $pageTitle = "About Us"; ?>
+<!DOCTYPE html>
+<html lang="la">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $pageTitle; ?></title>
+    <link rel="shortcut icon" href="../img/logo.jpeg" type="logo">
+    <link rel="stylesheet" href="../css/about.css">
+    <link rel="stylesheet" href="../css/social-buttons.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+    
+</head>
+
+<body>
+    <?php include 'header-en.html';?>
+
+<section class="about-section">
+        <div class="container">
+            <div class="heading-container text-center">
+                <h2>About <span>Us</span></h2>
+                    <p>Tech Art Language Translation Sole Co., Ltd is a private company operating under the direction and management of the Ministry of Information, Culture and Tourism in accordance with the Printing Law No. 05/NA, dated 09/12/2008. And it was established in compliance with Enterprise Law No. 46/NA, dated 26/12/2013.
+                    We provide professional translation services as including translation, proofreading, and certified correction from the original language to various foreign languages. Our team of expert translators boasts over five years of experience and holds advanced degrees, ensuring the highest level of quality and accuracy in every project.
+                </p>
+                <!-- <p>
+                    ບໍລິສັດເທັກອາດ ແປພາສາ ຕ່າງປະເທດ ຈຳກັດຜູ້ດຽວ ແມ່ນວິສະຫະກິດຂອງເອກະຊົນ ພາຍໃຕ
+                     ໄດ້ສ້າງຕັ້ງຖືກຕ້ອງຕາມກົດໝາຍ ວ່າດ້ວຍ ວິສາຫະກິດ ເລກທີ 46/ສພຊ, ລົງວັນທີ 26/12/2013 ແລະ ກົດໝາຍວ່າດ້ວຍ ການພິມຈຳໜ່າຍ ສະບັບເລກທີ 05/ສພຊ, ລົງວັນທີ 09/12/2008. ໂດຍການດຳເນີນທຸລະກິດ ພາຍໃຕການຄຸ້ມຄອງຂອງ ກະຊ່ວງກະແຫຼ່ງຂ່າວ, ວັດທະນະທຳ ແລະ ການທ່ອງທ່ຽວ ແລະ ລະບຽບກົດໝາຍຂອງ ສປປ ລາວ ວາງອອກແຕ່ລະໄລຍະ. ບໍລິສັດຂອງພວກເຮົາເປັນບໍລິສັດໜຶ່ງທີ່ໃຫ້ການບໍລິການ ດ້ານການແປພາສາເອກະສານ, ກວດກາ ແລະ ຢັ້ງຢືນຄວາມຖືກຕ້ອງ ຂອງພາສາຕ່າງປະເທດໃດໜຶ່ງ ຄຽງຄູ່ກັບທີມງານ ທີ່ມີປະສົບການສູງໃນການແປເອກະສານພາສາຕ່າງປະເທດ ໂດຍສະເພາະ ຫຼາຍກວ່າ 5 ກວ່າປີ ແລະ ຜູ້ທີ່ມີວຸດທິການສຶກສາ ລະດັບປະລິນຍາໂທ ຂຶ້ນໄປ.
+                </p> -->
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="image-box">
+                        <img src="../img/about.png" alt="ກ່ຽວກັບບໍລິສັດເທັກອາດ">
+                    </div>
+                </div>
+                <div class="col-md-6">
+                <h2>Vision and Mission</h2>
+                <div class="details-box">
+                    <p>
+                        <strong>Vision:</strong> The company is dedicated to delivering unparalleled document consultation and translation services that inspire confidence and consistently impress our clients.
+                    </p>
+                    <p>
+                        <strong>Mission:</strong> We are focused on delivering professional translation services that consistently impress our clients. Our commitment extends to the continuous development of our human resources and state-of-the-art translation tools, ensuring we remain modern and agile in an ever-changing global landscape.
+                    </p>
+                    <a href="maps.html" class="btn btn-primary">Contact Us</a>
+                        <div class="social-icons">
+                            <div class="social-icons">
+                                <a href="#" class="social-icon" title="WhatsApp"><i class="fab fa-whatsapp"></i></a>
+                                <a href="#" class="social-icon" title="Facebook"><i class="fab fa-facebook-f"></i></a>
+                                <a href="mailto:your@email.com" class="social-icon" title="Email"><i class="fas fa-envelope"></i></a>
+                                <a href="#" class="social-icon" title="TikTok"><i class="fab fa-tiktok"></i></a>
+                            </div>
+                        </div>
+                   
+                </div>
+            </div>
+        </div>
+    </section>
+    <?php include 'footer-en.html';?>
+</body>
+</html>
