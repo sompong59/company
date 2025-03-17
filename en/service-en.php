1daa953a889776c9c57764094e172f6e85b63cbe
sompong59 <102735484+sompong59@users.noreply.github.com>
2025-03-17 10:16:09 +0700

    add service-en.php

---
+++ en/service-en.php
@@ -0,0 +1,127 @@
+<?php $pageTitle = "ປະເພດການບໍລິການ"; ?>
+<!DOCTYPE html>
+<html lang="la">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $pageTitle; ?></title>
+    <link rel="shortcut icon" href="../img/logo.jpeg" type="logo">
+    <link rel="stylesheet" href="../css/service.css">
+    <link rel="stylesheet" href="../css/social-buttons.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+    
+</head>
+
+<body>
+    <?php include 'header-en.html';?>
+<div class="service-container">
+    <div class="translate-top-section">
+    <div class="translate-top-section-content">
+    <h2>What is Certified Document Translation?</h2>
+    <p>Certified document translation is intended for documents used in official capacities, where the recipient requires confirmation that the translation is accurate and complete. Such documents are frequently used in various locations, such as immigration offices, government agencies, and other places, both locally and internationally.</p>
+</div>
+        </div>
+    <div class="containercard">
+       
+        <div class="service-card">
+            <h2>General Documents</h2>
+           
+            <ul>
+                <li><i class="fas fa-check-circle"></i> Identity Card</li>
+                <li><i class="fas fa-check-circle"></i> Family Registration Book</li>
+                <li><i class="fas fa-check-circle"></i> Advertising and Marketing </li>
+                <li><i class="fas fa-check-circle"></i> Any documents that need translation</li>
+       
+            </ul>
+            <button class="quote-button">Get a Free Quote</button>
+        </div>
+        <div class="service-card">
+            <h2>Certification </h2>
+           
+            <ul>
+                <li><i class="fas fa-check-circle"></i> Enterprise Registration Certificate</li>
+                <li><i class="fas fa-check-circle"></i> Investment License </li>
+                <li><i class="fas fa-check-circle"></i> Business Operation License</li>
+                <li><i class="fas fa-check-circle"></i> Proposal Letter</li>
+                <li><i class="fas fa-check-circle"></i> Proof of Address</li>
+                <li><i class="fas fa-check-circle"></i> Marriage Certificate</li>
+                <li><i class="fas fa-check-circle"></i> Tax payment Certificate</li>
+                <li><i class="fas fa-check-circle"></i> Employee Certificate</li>
+                <li><i class="fas fa-check-circle"></i> Birth Certificate</li>
+                <li><i class="fas fa-check-circle"></i> Criminal Record</li>
+            </ul>
+            <button class="quote-button">Get a Free Quote</button>
+        </div>
+        <div class="service-card">
+            <h2>Legal documents</h2>
+            
+            <ul>
+                <li><i class="fas fa-check-circle"></i> Minute of Meeting</li>
+                <li><i class="fas fa-check-circle"></i> AOA</li>
+                <li><i class="fas fa-check-circle"></i> Concession Agreement</li>
+                <li><i class="fas fa-check-circle"></i> Power Purchase Agreement  </li>
+                <li><i class="fas fa-check-circle"></i> Cooperation Agreement</li>
+                <li><i class="fas fa-check-circle"></i> Shareholder Agreement</li>
+                <li><i class="fas fa-check-circle"></i> Sale Shareholder Agreement </li>
+                <li><i class="fas fa-check-circle"></i> Service Contract </li>
+                <li><i class="fas fa-check-circle"></i> Consultation Contract </li>
+                <li><i class="fas fa-check-circle"></i> Contractor Contract</li>
+                <li><i class="fas fa-check-circle"></i> Project development Agreement (PDA)</li>
+            </ul>
+            <button class="quote-button">Get a Free Quote</button>
+        </div>
+        <div class="service-card">
+            <h2>Certification</h2>
+            
+            <ul>
+                <li><i class="fas fa-check-circle"></i> Student Curriculum Vitae </li>
+                <li><i class="fas fa-check-circle"></i> Employee Curriculum Vitae </li>
+                <li><i class="fas fa-check-circle"></i> Government Application Form</li>
+                <li><i class="fas fa-check-circle"></i> Company Application Form</li>
+                <li><i class="fas fa-check-circle"></i> Visa Application Form in domestic and Aboard</li>
+           
+            </ul>
+            <button class="quote-button">Get a Free Quote</button>
+        </div>
+       
+    
+    </div>
+</div>
+
+<nav class="language-selector">
+    <h2>Supports Multiple Language Translations</h2>
+    <div class="language-container">
+        <a href="lao.html" class="language-item">
+            <img src="img/Lao.png" alt="Lao Flag">
+            <span>Lao</span>
+        </a>
+        <a href="thai.html" class="language-item">
+            <img src="img/Thai.png" alt="Thai Flag">
+            <span>Thai</span>
+        </a>
+        <a href="english.html" class="language-item">
+            <img src="img/english.png" alt="English Flag">
+            <span>English</span>
+        </a>
+        <a href="russian.html" class="language-item">
+            <img src="img/russia.png" alt="Russian Flag">
+            <span>Russian</span>
+        </a>
+        <a href="chinese.html" class="language-item">
+            <img src="img/chaina.png" alt="Chinese Flag">
+            <span>Chinese</span>
+        </a>
+        <a href="vietnamese.html" class="language-item">
+            <img src="img/vietnam.png" alt="Vietnamese Flag">
+            <span>Vietnamese</span>
+        </a>
+        <a href="french.html" class="language-item">
+            <img src="img/franc.png" alt="French Flag">
+            <span>French</span>
+        </a>
+    </div>
+</nav>
+</nav>
+<?php include 'footer-en.html';?>
+</body>
+</html>
