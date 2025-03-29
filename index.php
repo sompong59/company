3146cb76acf8e5c44d0d0743adcccc9c78b0c8e5
sompong59 <102735484+sompong59@users.noreply.github.com>
2025-03-29 22:34:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -20,67 +20,36 @@
 <body>
 <?php include 'header.html';?>
 
-<section class="herologo">
-        <div class="hero-image">
-            <img src="img/logobody.png" alt="ໂລໂກ້ເທັກອາດ">
+<div class="grid-container">
+        <div class="image-box">
+            <img src="img/bodytrans.png" alt="ກ່ຽວກັບບໍລິສັດເທັກອາດ">
         </div>
-        <div class="hero-content">
-            <div class="company-logo">
-                <img src="img/trans.png" alt="ໂລໂກ້">
-            </div>
-            <h1>"ຄວາມຖືກຕ້ອງ, ຄວາມເຊື່ອໝັ້ນ, ແລະຄວາມວ່ອງໄວ: ໃຫ້ເຮົາແປພາສາໃຫ້ທ່ານ."</h1>
-            <a href="maps.php">
-            <button class="consultation-btn">ສອບຖາມຂໍ້ມູນເບື້ອງຕົ້ນ</button></a>
-            <!-- <div class="hero-social-icons">
-            <a href="#"><i class="hero-icon-instagram"></i></a>
-           
-            <a href="#"><i class="hero-icon-facebook"></i></a>
-            <a href="#"><i class="hero-icon-twitter"></i></a>
-        </div> -->
-        </div>
-    </section>
-
-    <section class="about-section">
-        <div class="container">
-            <div class="heading-container text-center">
-                <h2>ກ່ຽວກັບ <span>ບໍລິສັດເທັກອາດ</span></h2>
-                <p>
+        <div class="text-content">
+            <h2>ກ່ຽວກັບ <span>ບໍລິສັດເທັກອາດ</span></h2>
+            <p>
                 ບໍລິສັດ ເທັກອາດ ແປພາສາ ຕ່າງປະເທດ ຈຳກັດຜູ້ດຽວ ແມ່ນວິສາຫະກິດຂອງເອກະຊົນ ພາຍໃຕ້ການຊີ້ນຳຄຸ້ມຄອງໂດຍ ກະຊວງຖະແຫຼ່ງຂ່າວ, ວັດທະນະທຳ ແລະ ການທ່ອງທ່ຽວ ຕາມກົດໝາຍວ່າດ້ວຍການພິມຈຳໜ່າຍ ສະບັບເລກທີ 05/ສພຊ, ລົງວັນທີ 09/12/2008 ແລະ ໄດ້ສ້າງຕັ້ງຖືກຕ້ອງຕາມກົດໝາຍ ວ່າດ້ວຍ ວິສາຫະກິດ ເລກທີ 46/ສພຊ, ລົງວັນທີ 26/12/2013. 
-ພວກເຮົາເປັນບໍລິສັດໜຶ່ງທີ່ໃຫ້ການບໍລິການ ດ້ານການແປພາສາເອກະສານ, ກວດກາ ແລະ ຢັ້ງຢືນຄວາມຖືກຕ້ອງ ຂອງພາສາຕ່າງປະເທດ ຄຽງຄູ່ກັບທີມງານ ທີ່ມີຄວາມຊ່ຽວຊານ ໃນການແປເອກະສານພາສາຕ່າງປະເທດ, ແລະ ປະສົບການ ຫຼາຍກວ່າ 5ປີ ແລະ ມີວຸດທິການສຶກສາ ລະດັບປະລິນຍາໂທ ຂຶ້ນໄປ.
-
-                </p>
-            </div>
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="image-box">
-                        <img src="img/about.png" alt="ກ່ຽວກັບບໍລິສັດເທັກອາດ">
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <h2>ວິໄສທັດ ແລະ ພາລະກິດ</h2>
-                    <div class="details-box">
-
-                        <p>
-                            <strong>ວິໄສທັດ:</strong> ເປັນບໍລິສັດ ທີ່ສ້າງຄວາມເຊື່ອໝັ້ນໃນການບໍລິການ, ທີ່ປຶກສາ ດ້ານເອກະສານ ແລະ ແປພາສາຕ່າງປະເທດ ໃຫ້ລູກຄ້າຢ່າງເພີ່ງພໍໃຈ.
-                        </p>
-                        <p>
-                            <strong>ພາລະກິດ:</strong> ເນັ້ນໜັກ ແລະ ຍຶດໝັ້ນການບໍລິການດ້ານການແປພາສາຢາງມືອາຊີບ, ເນັ້ນສ້າງ ຄວາມປະທັບໃຈຕໍ່ລູກຄ້າທຸກທ່ານທີ່ມາໃຊ້ບໍລິການ, ມຸ່ງໝັ້ນພັດທະນາບຸກຄະລະກອນ ແລະ ເຄື່ອງມືການບໍລິການພາສາຕ່າງປະເທດໃຫ້ທັນສະໄໝ ແລະ ສາມາດຕອບສະໜອງການປ່ຽນແປງ ຂອງ ວິວັດທະນາການໃນແຕ່ລະໄລຍະ.
-                        </p>
-                       
-                        <a href="maps.php" class="button-primary">ຕິດຕໍ່ເຮົາ</a>
-                        <div class="social-icons">
-                            <div class="social-icons">
-                                <a href="#" class="social-icon" title="WhatsApp"><i class="fab fa-whatsapp"></i></a>
-                                <a href="#" class="social-icon" title="Facebook"><i class="fab fa-facebook-f"></i></a>
-                                <a href="mailto:your@email.com" class="social-icon" title="Email"><i class="fas fa-envelope"></i></a>
-                                <a href="#" class="social-icon" title="TikTok"><i class="fab fa-tiktok"></i></a>
-                            </div>
-                        </div>
-                   
+                ພວກເຮົາເປັນບໍລິສັດໜຶ່ງທີ່ໃຫ້ການບໍລິການ ດ້ານການແປພາສາເອກະສານ, ກວດກາ ແລະ ຢັ້ງຢືນຄວາມຖືກຕ້ອງ ຂອງພາສາຕ່າງປະເທດ ຄຽງຄູ່ກັບທີມງານ ທີ່ມີຄວາມຊ່ຽວຊານ ໃນການແປເອກະສານພາສາຕ່າງປະເທດ, ແລະ ປະສົບການ ຫຼາຍກວ່າ 5ປີ ແລະ ມີວຸດທິການສຶກສາ ລະດັບປະລິນຍາໂທ ຂຶ້ນໄປ.
+            </p>
+    
+            <h2>ວິໄສທັດ ແລະ ພາລະກິດ</h2>
+            <p>
+                <strong>ວິໄສທັດ:</strong> ເປັນບໍລິສັດ ທີ່ສ້າງຄວາມເຊື່ອໝັ້ນໃນການບໍລິການ, ທີ່ປຶກສາ ດ້ານເອກະສານ ແລະ ແປພາສາຕ່າງປະເທດ ໃຫ້ລູກຄ້າຢ່າງເພີ່ງພໍໃຈ.
+            </p>
+            <p>
+                <strong>ພາລະກິດ:</strong> ເນັ້ນໜັກ ແລະ ຍຶດໝັ້ນການບໍລິການດ້ານການແປພາສາຢາງມືອາຊີບ, ເນັ້ນສ້າງ ຄວາມປະທັບໃຈຕໍ່ລູກຄ້າທຸກທ່ານທີ່ມາໃຊ້ບໍລິການ, ມຸ່ງໝັ້ນພັດທະນາບຸກຄະລະກອນ ແລະ ເຄື່ອງມືການບໍລິການພາສາຕ່າງປະເທດໃຫ້ທັນສະໄໝ ແລະ ສາມາດຕອບສະໜອງການປ່ຽນແປງ ຂອງ ວິວັດທະນາການໃນແຕ່ລະໄລຍະ.
+            </p>
+    
+            <div class="contact-info">
+                <a href="maps.php" class="button-primary">ຕິດຕໍ່ພວກເຮົາ</a>
+                <div class="social-icons">
+                    <a href="#" class="social-icon" title="WhatsApp"><i class="fab fa-whatsapp"></i></a>
+                    <a href="#" class="social-icon" title="Facebook"><i class="fab fa-facebook-f"></i></a>
+                    <a href="mailto:your@email.com" class="social-icon" title="Email"><i class="fas fa-envelope"></i></a>
+                    <a href="#" class="social-icon" title="TikTok"><i class="fab fa-tiktok"></i></a>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
 
     <div class="service-container">
     <div class="translate-top-section">
